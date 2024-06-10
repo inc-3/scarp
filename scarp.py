db973752969065fbe4d7b6b65d1b5b6b2a6a5048
INC3PTION <91461469+inc-3@users.noreply.github.com>
2024-06-10 23:55:19 +0600

    Update scarp.py

--- scarp.py
+++ scarp.py
@@ -82,79 +82,14 @@ class Menu():
     def __init__(self):
         self.result = []
         print('[ Dump Menu ]')
-        print('[1] Friendlist')
-        print('[2] Followers')
-        print('[3] Member Group')
-        print('[4] Search Name')
         print('[5] Post Reactions')  # Add this option
         print('[0] Logout')
         x = input('Pilih : ')
         print('')
         if   x in ['0','00','000','z']: Logout()
-        elif x in ['1','01','001','a']: self.MenuFriendlist()
-        elif x in ['2','02','002','b']: self.MenuFollowers()
-        elif x in ['3','03','003','c']: self.MenuMemberGroup()
-        elif x in ['4','04','004','d']: self.MenuSearchName()
         elif x in ['5','05','005','e']: self.MenuPostReactions()  # Add this option
         else: print('Isi Yang Benar!'); exit()
         print('\rBerhasil Dump %s ID'%(str(len(self.result))))
-
-    #--> Menu Friendlist
-    def MenuFriendlist(self):
-        DP = Dump()
-        p = input('Masukkan ID/URL Akun Target : ').split(',')
-        print('')
-        print('Tekan ctrl+c Untuk Berhenti')
-        for lk in p:
-            try:
-                url = ConvertURL(lk)
-                DP.Friendlist(url,self.result)
-            except Exception as e:
-                print('Target : %s'%(lk))
-                print('Error, Something Went Wrong!\n')
-    
-    #--> Menu Followers
-    def MenuFollowers(self):
-        DP = Dump()
-        p = input('Masukkan ID/URL Akun Target : ').split(',')
-        print('')
-        print('Tekan ctrl+c Untuk Berhenti')
-        for lk in p:
-            try:
-                url = ConvertURL(lk)
-                DP.Followers(url,self.result)
-            except Exception as e:
-                print('Target : %s'%(lk))
-                print('Error, Something Went Wrong!\n')
-
-    #--> Menu Member Group
-    def MenuMemberGroup(self):
-        DP = Dump()
-        p = input('Masukkan ID/URL Grup Target : ').split(',')
-        print('')
-        print('Tekan ctrl+c Untuk Berhenti')
-        for lk in p:
-            try:
-                url = ConvertURL(lk)
-                DP.MemberGroup(url,self.result)
-            except Exception as e:
-                print('Target : %s'%(lk))
-                print('Error, Something Went Wrong!\n')
-
-    #--> Menu Search Name
-    def MenuSearchName(self):
-        DP = Dump()
-        p = input('Masukkan Nama : ').split(',')
-        print('')
-        print('Tekan ctrl+c Untuk Berhenti')
-        for lk in p:
-            try:
-                nm = lk.replace(' ','+')
-                url = f'https://www.facebook.com/search/people/?q={nm}'
-                DP.SearchName(url,self.result)
-            except Exception as e:
-                print('Target : %s'%(lk))
-                print('Error, Something Went Wrong!\n')
 
     #--> Menu Post Reactions
     def MenuPostReactions(self):
@@ -203,168 +138,10 @@ class Dump():
         self.loop = 1
         self.cookie = open('login/cookie.json','r').read()
 
-    #--> Dump ID From Friendlist
-    def Friendlist(self, url, result, r=None, dta=None, cur=None, id_post=None):
-        if r==None and cur==None:
-            r = requests.Session()
-            req = bs(r.get(url,headers=HeadersGet(),cookies={'cookie':self.cookie},allow_redirects=True).content,'html.parser')
-            try:
-                dta = GetData(req)
-                id_post = re.search('{"tab_key":"friends_all","id":"(.*?)"}',str(req)).group(1)
-                target_id = re.search('"userID":"(.*?)"',str(req)).group(1)
-                target_name = re.search('"__isProfile":"User","name":"(.*?)"',str(req)).group(1)
-                print('ID : %s'%(target_id))
-                print('Name : %s'%(target_name))
-            except Exception as e:
-                print('URL : %s'%(url))
-                print('Friendlist Private Atau ID Tidak Ditemukan!')
-                exit()
-        var = {"count":8,"cursor":cur,"scale":1.5,"search":None,"id":id_post}
-        dta.update({'fb_api_caller_class':'RelayModern','fb_api_req_friendly_name':'ProfileCometAppCollectionListRendererPaginationQuery','variables':json.dumps(var),'server_timestamps':True,'doc_id': '6767163196701249'})
-        pos = r.post('https://www.facebook.com/api/graphql/',data=dta,headers=HeadersPost(),cookies={'cookie':self.cookie}).json()
-        for x in pos['data']['node']['pageItems']['edges']:
-            try:
-                id   = x['node']['actions_renderer']['action']['client_handler']['profile_action']['restrictable_profile_owner']['id']
-                name = x['node']['actions_renderer']['action']['client_handler']['profile_action']['restrictable_profile_owner']['name']
-                user = x['node']['url'].split('/')[-1].replace('profile.php?id=','')
-                info = x['node']['subtitle_text']['text']
-                plot = '%s|%s|%s|%s'%(id,user,name,info)
-                if plot in result: pass
-                else: result.append(plot)
-                print('\rSedang Dump %s ID'%(str(len(result))),end=''); sys.stdout.flush()
-            except KeyboardInterrupt: self.loop=0; pass
-            except Exception as e: pass
-        try:
-            jut = pos['data']['node']['pageItems']['page_info']
-            if jut['has_next_page'] and self.loop:
-                cur = jut['end_cursor']
-                self.Friendlist(url, result, r, dta, cur, id_post)
-        except KeyboardInterrupt: self.loop=0; pass
-        except Exception as e: pass
-
-    #--> Dump ID From Followers
-    def Followers(self, url, result, r=None, dta=None, cur=None, id_post=None):
-        if r==None and cur==None:
-            r = requests.Session()
-            req = bs(r.get(url,headers=HeadersGet(),cookies={'cookie':self.cookie},allow_redirects=True).content,'html.parser')
-            try:
-                dta = GetData(req)
-                id_post = re.search('{"tab_key":"followers","id":"(.*?)"}',str(req)).group(1)
-                target_id = re.search('"userID":"(.*?)"',str(req)).group(1)
-                target_name = re.search('"__isProfile":"User","name":"(.*?)"',str(req)).group(1)
-                print('ID : %s'%(target_id))
-                print('Name : %s'%(target_name))
-            except Exception as e:
-                print('URL : %s'%(url))
-                print('Followers Private Atau ID Tidak Ditemukan!')
-                exit()
-        var = {"count":8,"cursor":cur,"scale":1.5,"search":None,"id":id_post}
-        dta.update({'fb_api_caller_class':'RelayModern','fb_api_req_friendly_name':'ProfileCometAppCollectionListRendererPaginationQuery','variables':json.dumps(var),'server_timestamps':True,'doc_id': '6767163196701249'})
-        pos = r.post('https://www.facebook.com/api/graphql/',data=dta,headers=HeadersPost(),cookies={'cookie':self.cookie}).json()
-        for x in pos['data']['node']['pageItems']['edges']:
-            try:
-                id   = x['node']['actions_renderer']['profile_actions'][0]['client_handler']['profile_action']['restrictable_profile_owner']['id']
-                name = x['node']['actions_renderer']['profile_actions'][0]['client_handler']['profile_action']['restrictable_profile_owner']['name']
-                user = x['node']['url'].split('/')[-1].replace('profile.php?id=','')
-                info = x['node']['subtitle_text']['text']
-                plot = '%s|%s|%s|%s'%(id,user,name,info)
-                if plot in result: pass
-                else: result.append(plot)
-                print('\rSedang Dump %s ID'%(str(len(result))),end=''); sys.stdout.flush()
-            except KeyboardInterrupt: self.loop=0; pass
-            except Exception as e: pass
-        try:
-            jut = pos['data']['node']['pageItems']['page_info']
-            if jut['has_next_page'] and self.loop:
-                cur = jut['end_cursor']
-                self.Followers(url, result, r, dta, cur, id_post)
-        except KeyboardInterrupt: self.loop=0; pass
-        except Exception as e: pass
-
-    #--> Dump ID From Members Group
-    def MemberGroup(self, url, result, r=None, dta=None, cur=None, id_group=None):
-        if r==None and cur==None:
-            r = requests.Session()
-            req = bs(r.get(url,headers=HeadersGet(),cookies={'cookie':self.cookie}).content,'html.parser')
-            try:
-                dta = GetData(req)
-                id_group = re.search('"groupID":"(.*?)"',str(req)).group(1)
-                name_group = re.search('"meta":{"title":"(.*?)","accessory"',str(req)).group(1)
-                print('ID : %s'%(id_group))
-                print('Name : %s'%(name_group))
-            except Exception as e:
-                print('URL : %s'%(url))
-                print('Group Private Atau ID Tidak Ditemukan!')
-                exit()
-        var = {"count":10,"cursor":cur,"groupID":id_group,"recruitingGroupFilterNonCompliant":False,"scale":1.5,"id":id_group}
-        dta.update({'fb_api_caller_class':'RelayModern','fb_api_req_friendly_name':'GroupsCometMembersPageNewMembersSectionRefetchQuery','variables':json.dumps(var),'server_timestamps':True,'doc_id':'6621621524622624'})
-        pos = r.post('https://www.facebook.com/api/graphql/',data=dta,headers=HeadersPost(),cookies={'cookie':self.cookie}).json()
-        for x in pos['data']['node']['new_members']['edges']:
-            try:
-                id   = x['node']['id']
-                name = x['node']['name']
-                user = x['node']['url'].split('/')[-1].replace('profile.php?id=','')
-                info = x['node']['bio_text']['text']
-                plot = '%s|%s|%s|%s'%(id,user,name,info)
-                if plot in result: pass
-                else: result.append(plot)
-                print('\rSedang Dump %s ID'%(str(len(result))),end=''); sys.stdout.flush()
-            except KeyboardInterrupt: self.loop=0; pass
-            except Exception as e: pass
-        try:
-            jut = pos['data']['node']['new_members']['page_info']
-            if jut['has_next_page'] and self.loop:
-                cur = jut['end_cursor']
-                self.MemberGroup(url, result, r, dta, cur, id_group)
-        except KeyboardInterrupt: self.loop=0; pass
-        except Exception as e: pass
-    
-    #--> Dump ID From Search Name
-    def SearchName(self, url, result, r=None, dta=None, cur=None, bsid=None):
-        if r==None and cur==None:
-            r = requests.Session()
-            req = bs(r.get(url,headers=HeadersGet(),cookies={'cookie':self.cookie}).content,'html.parser')
-            try:
-                dta = GetData(req)
-                bsid = re.search('"bsid":"(.*?)"',str(req)).group(1)
-            except Exception as e:
-                print('URL : %s'%(url))
-                print('Nama Tidak Ditemukan!')
-                exit()
-        var = {"UFI2CommentsProvider_commentsKey":"SearchCometResultsInitialResultsQuery","allow_streaming":False,"args":{"callsite":"COMET_GLOBAL_SEARCH","config":{"exact_match":False,"high_confidence_config":None,"intercept_config":None,"sts_disambiguation":None,"watch_config":None},"context":{"bsid":bsid,"tsid":None},"experience":{"encoded_server_defined_params":None,"fbid":None,"type":"PEOPLE_TAB"},"filters":[],"text":url.split('=')[-1].replace('+',' ')},"count":5,"cursor":cur,"displayCommentsContextEnableComment":False,"displayCommentsContextIsAdPreview":False,"displayCommentsContextIsAggregatedShare":False,"displayCommentsContextIsStorySet":False,"displayCommentsFeedbackContext":None,"feedLocation":"SEARCH","feedbackSource":23,"fetch_filters":True,"focusCommentID":None,"locale":None,"privacySelectorRenderLocation":"COMET_STREAM","renderLocation":"search_results_page","scale":1.5,"stream_initial_count":0,"useDefaultActor":False,"__relay_internal__pv__IsWorkUserrelayprovider":False,"__relay_internal__pv__IsMergQAPollsrelayprovider":False,"__relay_internal__pv__CometUFIReactionsEnableShortNamerelayprovider":False,"__relay_internal__pv__CometUFIIsRTAEnabledrelayprovider":False,"__relay_internal__pv__StoriesArmadilloReplyEnabledrelayprovider":False,"__relay_internal__pv__StoriesRingrelayprovider":False}
-        dta.update({'fb_api_caller_class':'RelayModern','fb_api_req_friendly_name':'SearchCometResultsPaginatedResultsQuery','variables':json.dumps(var),'server_timestamps':True,'doc_id':'7704205549605925'})
-        pos = r.post('https://www.facebook.com/api/graphql/',data=dta,headers=HeadersPost(),cookies={'cookie':self.cookie}).json()
-        for x in pos['data']['serpResponse']['results']['edges']:
-            try:
-                id   = x['relay_rendering_strategy']['view_model']['loggedProfile']['id']
-                name = x['relay_rendering_strategy']['view_model']['loggedProfile']['name']
-                user = x['relay_rendering_strategy']['view_model']['loggedProfile']['url'].split('/')[-1].replace('profile.php?id=','')
-                info = x['relay_rendering_strategy']['view_model']['primary_snippet_text_with_entities']['text']
-                plot = '%s|%s|%s|%s'%(id,user,name,info)
-                if plot in result: pass
-                else: result.append(plot)
-                print('\rSedang Dump %s ID'%(str(len(result))),end=''); sys.stdout.flush()
-            except KeyboardInterrupt: self.loop=0; pass
-            except Exception as e: pass
-        try:
-            jut = pos['data']['serpResponse']['results']['page_info']
-            if jut['has_next_page'] and self.loop:
-                cur = jut['end_cursor']
-                self.SearchName(url, result, r, dta, cur, bsid)
-        except KeyboardInterrupt: self.loop=0; pass
-        except Exception as e: pass
-
-def PostReactions(self, post_url, result):
-    r = requests.Session()
-    req = r.get(post_url, headers=HeadersGet(), cookies={'cookie': self.cookie}, allow_redirects=True).content
-    
-    # Perform the regular expression search
-    post_id_match = re.search('"fb://(\d+)"', str(req))
-    
-    # Check if a match was found
-    if post_id_match:
-        # Access the group if a match was found
-        post_id = post_id_match.group(1)
+    def PostReactions(self, post_url, result):
+        r = requests.Session()
+        req = r.get(post_url, headers=HeadersGet(), cookies={'cookie': self.cookie}, allow_redirects=True).content
+        post_id = re.search('"fb://(\d+)"', str(req)).group(1)
         
         # Construct the GraphQL query to fetch post reactions
         query = '''
@@ -391,8 +168,6 @@ def PostReactions(self, post_url, result):
         
         print('\rSedang Dump %s ID' % str(len(result)), end='')
         sys.stdout.flush()
-    else:
-        print("No post ID found in the response.")
 
 #--> Trigger
 if __name__ == '__main__':
