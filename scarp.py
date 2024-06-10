5fbc82eba22402411c9d68dc0374a641a5b3c1d1
INC3PTION <91461469+inc-3@users.noreply.github.com>
2024-06-11 00:47:36 +0600

    Update scarp.py

--- scarp.py
+++ scarp.py
@@ -1,59 +1,96 @@
+import os
 import requests
-import json
 import re
-import sys
+from bs4 import BeautifulSoup as parser
 
-#--> Input Cookies Function
-def input_cookies():
-    return input('Input Cookies: ')
+class Login:
+    def __init__(self):
+        self.token = ""
+        self.cookies = self.load_cookies()
+        if self.cookies:
+            self.validate_cookies()
+        else:
+            self.get_new_cookies()
 
-#--> Input Post ID Function
-def input_post_id():
-    return input('Input Post ID: ')
+    def load_cookies(self):
+        try:
+            with open('cookies.txt', 'r') as file:
+                cookies = file.read().strip()
+            return cookies
+        except FileNotFoundError:
+            return None
 
-#--> PostReactions Method
-def PostReactions(self, post_url, result):
-    # Get cookies from input
-    self.cookie = input_cookies()
-    
-    r = requests.Session()
-    req = r.get(post_url, headers=HeadersGet(), cookies={'cookie': self.cookie}, allow_redirects=True).content
-    
-    # Perform the regular expression search
-    post_id_match = re.search('"fb://(\d+)"', str(req))
-    
-    # Check if a match was found
-    if post_id_match:
-        # Access the group if a match was found
-        post_id = post_id_match.group(1)
-        
-        # Get post ID from input
-        # post_id = input_post_id()
-        
-        # Construct the GraphQL query to fetch post reactions
-        query = '''
-        {
-          node(id: "''' + post_id + '''") {
-            id
-            reactions(first: 100) {
-              nodes {
-                id
-              }
-            }
-          }
-        }
-        '''
-        
-        # Send the GraphQL request
-        response = r.post('https://www.facebook.com/api/graphql/', data={'fb_api_req_friendly_name': 'SinglePostReactions', 'variables': json.dumps({}), 'doc_id': '3560935007346409', 'query': query}, headers=HeadersPost(), cookies={'cookie': self.cookie}).json()
-        
-        # Extract user IDs from the response
-        reactions = response['data']['node']['reactions']['nodes']
-        for reaction in reactions:
-            user_id = reaction['id']
-            result.append(user_id)
-        
-        print('\rSedang Dump %s ID' % str(len(result)), end='')
-        sys.stdout.flush()
-    else:
-        print("No post ID found in the response.")
+    def save_cookies(self, cookies):
+        with open('cookies.txt', 'w') as file:
+            file.write(cookies)
+
+    def validate_cookies(self):
+        response = requests.get('https://mbasic.facebook.com/profile.php', cookies={'cookie': self.cookies})
+        if 'mbasic_logout_button' in response.text:
+            self.token = self.cookies
+        else:
+            self.get_new_cookies()
+
+    def get_new_cookies(self):
+        self.cookies = input("Enter your Facebook cookies: ")
+        self.save_cookies(self.cookies)
+        self.validate_cookies()
+
+class Dump:
+    def __init__(self, token):
+        self.token = token
+
+    def dump_post_likes(self, post_id):
+        url = f"https://mbasic.facebook.com/ufi/reaction/profile/browser/?ft_ent_identifier={post_id}"
+        return self.scrape(url)
+
+    def scrape(self, url):
+        session = requests.Session()
+        response = session.get(url, cookies={'cookie': self.token})
+        soup = parser(response.text, 'html.parser')
+        users = []
+        for user in soup.find_all('a', href=True):
+            uid_match = re.search(r'id=(\d+)', user['href'])
+            if uid_match:
+                uid = uid_match.group(1)
+                name = user.get_text()
+                users.append(f"{uid}|{name}")
+            if len(users) >= 100:
+                break
+        return users
+
+    def save_cookies(self, cookies):
+        with open('cookies.txt', 'w') as file:
+            file.write(cookies)
+
+class Menu:
+    def __init__(self, dump):
+        self.dump = dump
+        self.run_menu()
+
+    def run_menu(self):
+        while True:
+            self.clear_screen()
+            print("Select an option:")
+            print("1. Dump Post Likes")
+            print("2. Exit")
+            choice = input("Enter your choice: ")
+            if choice == '1':
+                post_id = input("Enter post ID: ")
+                users = self.dump.dump_post_likes(post_id)
+                print("Found Users:")
+                for user in users:
+                    print(user)
+                input("Press Enter to continue...")
+            elif choice == '2':
+                break
+            else:
+                print("Invalid choice. Try again.")
+
+    def clear_screen(self):
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+if __name__ == "__main__":
+    login = Login()
+    dump = Dump(login.token)
+    menu = Menu(dump)
